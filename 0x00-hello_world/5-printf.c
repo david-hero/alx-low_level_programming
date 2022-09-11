086a32477a2ce0916d47ce9c2086215118d3b546
david-hero <agunwamba@gmail.com>
2022-09-11 15:25:30 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include  <stdio.h>
 
 /**                                                                                                                                     
 * main - print the string in the put function                                                                                           
@@ -9,6 +9,6 @@
 */                                                                                                                                      
 int main(void)                                                                                                                          
 {  
-        printf("with proper grammar, but the outcome is a piece of art, \n");
-	return (0);
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+	    return (0);
 }
