842d66aecebd8f007e0fd1132db75865a02782cb
david-hero <agunwamba@gmail.com>
2022-09-12 13:17:30 -0700

    I sometimes suffer from insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/**
+/*
  * main - Entry point
  *
  * Return: Always 0 (Success)
@@ -8,12 +8,13 @@
 int main(void)
 {
 char c = 'a';
-while (c <= 'z')
-{
-	     putchar(c);
-	     c++;
-}
-	     putchar('\n');
 
-return (0);
+	while (c <= 'z')
+{
+		putchar(c);
+		c++;
+}
+	putchar('\n');
+
+	return (0);
 }
