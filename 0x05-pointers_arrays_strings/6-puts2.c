09b5a0ef987e71512eeeee07c23362b705c267e2
david-hero <agunwamba@gmail.com>
2022-09-19 07:34:12 -0700

    Half the lies they tell about me arent true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,14 +10,17 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+int i = 0;
 
-	while (*(str + i) != '\0')
+while (*(str + i) != '\0')
+
 {
-	if (i % 2 == 0)
 
+if (i % 2 == 0)
+	
 		putchar(*(str + i));
-		i++;
+				i++;
 }
-	putchar(10);
+putchar(10);
+
 }
