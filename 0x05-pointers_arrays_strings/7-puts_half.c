1ef0624d52f64d737af3a3467bebf50222d0db41
david-hero <agunwamba@gmail.com>
2022-09-20 08:00:16 -0700

    Winning is only half of it

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,9 +11,10 @@
 void puts_half(char *str)
 {
 	int i, len = _strlen(str);
-		for (i = ((len - 1) / 2) + 1; i < len; i++)
-				putchar(*(str + i));
-		putchar(10);
+	
+	for (i = ((len - 1) / 2) + 1; i < len; i++)
+		putchar(*(str + i));
+	putchar(10);
 }
 
 /**
@@ -26,7 +27,9 @@ void puts_half(char *str)
 int _strlen(char *s)
 {
 	int len = 0;
-		while (*(s + len) != '\0')
-				len++;
-		return (len);
+		
+	while (*(s + len) != '\0')
+		len++;
+	
+	return (len);
 }
